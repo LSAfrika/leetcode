01b38e194151c41ec48b519b953e5e4893d6a89f
LSAfrika <lstudiosafrika@gmail.com>
2023-11-01 20:24:16 +0300

    added algorithims folder

--- data stractures/linkedlist.js
+++ data stractures/linkedlist.js
@@ -16,7 +16,8 @@ class LinkedList{
         return this.size===0
     }
 
-     linkedlistsize(){
+     Size(){
+        console.log('size of linkedlist',this.size);
         return this.size
      }
 
@@ -50,10 +51,13 @@ class LinkedList{
             let currentnode= this.head
             while(currentnode.next){
                 currentnode=currentnode.next
+
+                // console.log(currentnode);
             }
 
             currentnode.next=nodevalue
         }
+        this.size++
      }
 
      view(){
@@ -67,12 +71,146 @@ class LinkedList{
             currentnode=currentnode.next
         }
 
-        console.log(listvalue,'null');
+        console.log(listvalue,'null \n',);
      }
 
-     insert(value,index){
-        if(index<0||index>this.size) return console.log('index out of range');
-        if(index==0) this.prepend(value)
+     insertnode(value,index){
+        let previoucounter=1
+        if(index<0||index>this.size)  {console.log('index out of range');return;}
+        if(index==0){ this.prepend(value)}
+        if(index>0){ 
+            let insertionnode=new node(value)
+
+            let previousnode=this.head
+
+            while(previoucounter!=index-1){
+                previousnode=previousnode.next
+                previoucounter++
+
+            }
+             insertionnode.next=previousnode.next
+             previousnode.next=insertionnode
+
+             this.size++
+           //  console.log(`node at index ${index}\n`,'prev node value',previousnode,'\n',insertionnode);
+        
+        }
+
+     }
+
+     removenode(index){
+        if(index<0||index>this.size)  {console.log('remove node index out of range');return;}
+        if(index==0)this.head=this.head.next
+        if(index>0){
+            let counter=0
+            let currenthead=this.head
+            let previousnode
+            
+
+            while(counter<index){
+
+                if(counter==index-1)previousnode=currenthead
+                currenthead=currenthead.next
+                counter++
+            }
+
+            // console.log('previous head remove function',previousnode.value);
+            // console.log('current head remove function',currenthead.value);
+           // counter=1
+            previousnode.next=currenthead.next
+        }
+
+        this.size--
+        // console.log('size decrease:',this.size);
+
+        
+
+     }
+
+     //SHORT HAND WAY OF REMOVING FOM LIST WHILE REUSING DELETE NODE METHODE
+
+     removenodewithspecificvalue(value){
+
+        if(this.linkedlistempty())return console.log('list is empty');
+        let currentnode=this.head
+       console.log('current link size: ',this.size);
+        for (let i = 0; i < this.size; i++) {
+
+            // console.log('current node value',currentnode.value,i);
+            if(currentnode.value==value){
+
+                // console.log(i);
+                this.removenode(i);
+                return console.log(`${value} removed  from list at index ${i}`);
+            } 
+
+            currentnode=currentnode.next 
+                 
+        }
+
+        console.log(`${value} is not in the linked list`);
+     }
+
+     //WAY NUMBER 2 TO DELETE FROM LIST USING VALUE
+
+     removenodewithspecificvalue2(value){
+
+        if(this.linkedlistempty())return console.log('the link list is empty');
+         if(this.head.value===value) {
+            this.size--
+            return this.head=this.head.next
+        }else{
+            let currentnode=this.head
+            while (currentnode.next && currentnode.next.value!==value) {
+
+             //   console.log(`current node value: ${currentnode.value}`);
+                currentnode=currentnode.next
+                
+            }
+
+             //console.log(currentnode);
+            if(currentnode.next){
+
+            let nodetodelete =currentnode.next
+            currentnode.next=nodetodelete.next
+            this.size--
+            return console.log(`node with ${value} has been removed from linked list`)
+
+            }
+
+            return console.log(`${value} was not found in any node in the list`);
+
+
+        }
+
+        
+     }
+
+     searchlist(value){
+
+        if(this.linkedlistempty())return console.log('no search done since slist is empty');
+
+        let currentnode=this.head
+        let listscounter=0
+        let valueindex=[]
+     
+
+
+
+
+      
+        while (currentnode) {
+       
+            if(currentnode.value==value) valueindex.push(listscounter)
+            currentnode=currentnode.next
+            listscounter++
+            
+        }
+     
+
+        if (valueindex.length>0) return console.log(`the value ${value} occurs ${valueindex.length} time(s) at index [${valueindex}]`);
+            return console.log(` ${value} is not in any node in the list`);
+        
      }
 }
 
