b6baaef2fda0a705fa2fdc54a6d5d4a17b21c6a6
LSAfrika <lstudiosafrika@gmail.com>
2023-12-24 21:46:33 +0300

    advanced get added null check for given key string return undefined

--- data stractures/hashtable.js
+++ data stractures/hashtable.js
@@ -35,13 +35,13 @@ class HashTable{
     advancedset(key,value){
         let index= this.hash(key)
         let bucket=  this.table[index]
-        console.log('bucket set',bucket);
+      //s  console.log('bucket set',bucket);
 
         if(!bucket){
             bucket=[[key,value]] 
         }else{
             const samekeyhash=bucket.find(keyitem=>keyitem  [0]==key)
-            console.log('key hash: ',samekeyhash);
+            //console.log('key hash: ',samekeyhash);
             if(samekeyhash) {samekeyhash[1]=value}
             else{
 
@@ -63,7 +63,7 @@ class HashTable{
      //   console.log('adv get: ',bucket);
         if(!bucket) return undefined
       const valuesubarray=  bucket.find(keyitem=>keyitem[0]==key)
-
+      if(!valuesubarray) return undefined
       return valuesubarray[1]
     }
 
@@ -105,14 +105,13 @@ hashtable.advancedset('age',20)
 hashtable.advancedset('aeg',22)
 
 hashtable.display()
-// console.log(hashtable.get('age'));
-// console.log(hashtable.advancedget('name'));
+ console.log(hashtable.advancedget('names'));
 // //hashtable.remove('name')
 // console.log(hashtable.advancedget('mane'));
 // console.log(hashtable.advancedget('age'));
 hashtable.remove('aeg')
 hashtable.remove('maen')
 hashtable.remove('name')
-hashtable.display()
+//hashtable.display()
 
 
