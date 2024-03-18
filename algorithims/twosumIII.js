0eecc0cb732b0a8c75978b8ef0a54583c7b5d9b9
LSAfrika <lstudiosafrika@gmail.com>
2024-03-18 14:10:58 +0300

    two sum class redo for adding value and finding target in O(1) time'

--- algorithims/twosumIII.js
+++ algorithims/twosumIII.js
@@ -71,20 +71,90 @@ class twosum{
     }
 }
 
-const Tsum=new twosum()
-console.log(Tsum.stacksize);
-Tsum.add(1)
-Tsum.add(2)
-Tsum.add(3)
-Tsum.add(4)
-Tsum.add(5)
-Tsum.add(6)
-Tsum.add(7)
-Tsum.viewhashtable()
-Tsum.find(4)
-Tsum.find(7)
+// const Tsum=new twosum()
+// console.log(Tsum.stacksize);
+// Tsum.add(1)
+// Tsum.add(2)
+// Tsum.add(3)
+// Tsum.add(4)
+// Tsum.add(5)
+// Tsum.add(6)
+// Tsum.add(7)
+// // Tsum.viewhashtable()
+// Tsum.find(4)
+// Tsum.find(7)
 
 
 
+//self test create twosum class to add find a target value
 
 
+class TwoSumII{
+
+    constructor(){
+        this.stack=[]
+        this.lookup={}
+    }
+
+    add(value){
+        //guard clause for in values only
+        if(isNaN(value)==true) return console.log('input should be a number');
+        //guard clause for int string values 
+        value=parseInt(value)
+         const valueindex=  this.stack.indexOf(value)
+        if(valueindex==-1)  this.stack.push(value)
+        this.addtomap(value)
+
+    }
+
+
+    //issue with the look up function create combination for all sam totals
+    addtomap(value){
+
+        //  initial setup 
+           if(!this.lookup[value]) this.lookup[value]=[]
+      
+      
+
+               
+               
+                for(let i=0;i<this.stack.length;i++){
+                    let currentint= this.stack[i]
+                    if(currentint==value) continue
+                    let compliment=value-currentint
+                    let indexofcompliment=this.stack.indexOf(compliment)
+                    console.log(indexofcompliment);
+                    if(this.lookup[value].length>0) break
+                    if(indexofcompliment!=-1&&indexofcompliment!=i)this.lookup[value].push([i,indexofcompliment])
+                }
+               
+    
+
+    }
+
+    viewstack(){
+        return console.log(this.lookup,this.stack);
+    }
+
+    find(value){
+
+        if(!this.lookup[value]) return console.log(undefined);
+        if(this.lookup[value].length==0) return console.log([]);
+        return console.log(this.stack,this.lookup[value][0]);
+    }
+}
+
+
+const TsumII=new TwoSumII()
+
+TsumII.add(1)
+TsumII.add(2)
+
+TsumII.add(3)
+TsumII.add(4)
+TsumII.add(6)
+TsumII.viewstack()
+
+TsumII.find(5)
+TsumII.find(6)
+TsumII.find(7)
